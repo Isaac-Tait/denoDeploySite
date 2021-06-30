a96226de840f6d70efaede1f0945a277fc119818
Isaac Tait <isaac@mountaintopcoding.com>
2021-06-30 09:08:09 -0700

    add tailwindcss

--- deno_deploy_beta_app/src/App.js
+++ deno_deploy_beta_app/src/App.js
@@ -2,9 +2,9 @@ import logo from './logo.svg';
 
 function App() {
   return (
-    <div className="">
-      <header className="">
-        <img src={logo} className="" alt="logo" />
+    <div className="bg-gray-400 flex flex-col h-screen">
+      <header className="flex-grow font-semibold ml-20">
+        <img src={logo} className="h-40 w-40 flex flex-wrap mx-auto justify-center" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -17,6 +17,9 @@ function App() {
           Learn React
         </a>
       </header>
+      <footer className="ml-6">
+        <p>Another <a href="https://mountaintopcoding.dev" target="_blank" rel="noopener noreferrer" className="text-red-500 hover:text-blue-200 hover:bg-white">mountainTopCoding(<span role="img" aria-label="mountain with snow-cap">&#127956;</span>);</a> project</p>
+      </footer>
     </div>
   );
 }
